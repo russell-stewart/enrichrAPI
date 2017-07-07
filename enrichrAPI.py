571284dd6fe9851986e3f765e08873d4610186f1
Russell Stewart <russells98@gmail.com>
2017-07-07 14:37:06 -0600

    Add multiple module support and .xlsx file output.

--- enrichrAPI.py
+++ enrichrAPI.py
@@ -17,6 +17,7 @@ import requests
 import sys
 import getopt
 import operator
+import xlsxwriter
 
 #Stores all data associated with one result from a database. toString method outputs to CSV-style format.
 class Entry():
@@ -45,7 +46,7 @@ class Module():
         self.geneString = self.geneString + gene + '\n'
         self.numGenes += 1
     def toString(self):
-        return('Module Name: %s\n%s' % (self.name , self.geneString))
+        return('Module Name: %s\nNum. Genes:%d\n%s' % (self.name , self.numGenes , self.geneString))
 
 
 #this will be appended to become a database of all Modules
@@ -65,14 +66,11 @@ minOverlap = None
 for opt , arg in opts[0]:
     if opt == '--ifile':
         iFilePath = arg
-        print 'iFilePath = ' + iFilePath
     elif opt == '--ofile':
         oFilePath = arg
-        print 'oFilePath = ' + oFilePath
     elif opt == '--libraries':
         moreLibraries = True
         while moreLibraries:
-            print geneSetLibraries
             if arg.find(',') > -1:
                 geneSetLibraries.append(arg[:operator.indexOf(arg , ',')])
                 arg = arg[(operator.indexOf(arg  , ',') + 1):]
@@ -81,7 +79,6 @@ for opt , arg in opts[0]:
                 moreLibraries = False
     elif opt == '--minOverlap':
         minOverlap = arg
-        print 'minOverlap = ' + minOverlap
 
 if minOverlap is None:
     minOverlap = 0
@@ -96,72 +93,96 @@ numGenes = 0
 
 for line in ifile:
     gene = line[:line.find('\t')]
-    mod = line[(line.find('\t') + 1):]
-    hasBeenPlaced = False
-    for module in modules:
-        if module.name == mod:
-            module.add(gene)
-            hasBeenPlaced = True
-    if not hasBeenPlaced:
-        newMod = Module(mod)
-        newMod.add(gene)
-        modules.append(newMod)
+    mod = line[(line.find('\t') + 1):line.find('\n')]
+    if mod != 'module':
+        hasBeenPlaced = False
+        for module in modules:
+            if module.name == mod:
+                module.add(gene)
+                hasBeenPlaced = True
+        if not hasBeenPlaced:
+            newMod = Module(mod)
+            newMod.add(gene)
+            modules.append(newMod)
 
+ofile = xlsxwriter.Workbook(oFilePath)
+
+#go through ENRICHR API dance once per module, and add enriched data to ofile
 for module in modules:
-    print(module.toString())
+    print '\nEnriching module %s' % module.name
 
-ofile = open(oFilePath , 'w')
+    #Worksheet setup
+    worksheet = ofile.add_worksheet(module.name)
+    worksheet.write(0 , 0 , 'Gene Set')
+    worksheet.write(0 , 1 , 'Term')
+    worksheet.write(0 , 2 , 'Overlap')
+    worksheet.write(0 , 3 , 'Pval')
+    worksheet.write(0 , 4 , 'Z Score')
+    worksheet.write(0 , 5 , 'Adjusted Pval')
+    worksheet.write(0 , 6 , 'Combined Score')
+    worksheet.write(0 , 7 , 'Genes')
 
-#POST request to /Enrichr/addList to upload data
-payload = {
-    'list': (None, geneString)
-}
 
-print('Uploading data...')
+    #POST request to /Enrichr/addList to upload data
+    payload = {
+        'list': (None, module.geneString)
+    }
 
-response = requests.post(postURL , files=payload)
-if not response.ok:
-    raise Exception('Error analyzing gene list')
+    print('Uploading data...')
 
-print('Data uploaded.')
-
-uploadData = json.loads(response.text)
-
-ofile.write('Gene Set,Term,Overlap,Pval,Z Score,Adjusted Pval,Combined Score,Genes\n')
-
-#Iterates over all libraries in geneSetLibraries (list inputted by user), makes
-# a GET call to Enrichr/enrich, and transfers all results into entries
-for geneSetLibrary in geneSetLibraries:
-    print('Searching %s...' % geneSetLibrary)
-
-    response = requests.get('http://amp.pharm.mssm.edu/Enrichr/enrich?userListId=%s&backgroundType=%s' % (uploadData.get('userListId') , geneSetLibrary))
-
+    response = requests.post(postURL , files=payload)
     if not response.ok:
-        raise Exception('Error searching %s' % geneSetLibrary)
+        raise Exception('Error analyzing gene list')
 
-    data = json.loads(response.text)
+    print('Data uploaded.')
+
+    uploadData = json.loads(response.text)
+
+
+    #Iterates over all libraries in geneSetLibraries (list inputted by user), makes
+    # a GET call to Enrichr/enrich, and transfers all results into entries
+    for geneSetLibrary in geneSetLibraries:
+        print('Searching %s...' % geneSetLibrary)
+
+        response = requests.get('http://amp.pharm.mssm.edu/Enrichr/enrich?userListId=%s&backgroundType=%s' % (uploadData.get('userListId') , geneSetLibrary))
+
+        if not response.ok:
+            raise Exception('Error searching %s' % geneSetLibrary)
+
+        data = json.loads(response.text)
 
 
 
-    for database in data:
-        for entry in data[database]:
-            overlapGenes = ''
-            for gene in entry[5]:
-                overlapGenes += gene + ';'
-            entries.append(Entry(database , entry[1] , len(entry[5]) , entry[2] , entry[3] , entry[6] , entry[4] , overlapGenes))
+        for database in data:
+            for entry in data[database]:
+                overlapGenes = ''
+                for gene in entry[5]:
+                    overlapGenes += gene + ';'
+                entries.append(Entry(database , entry[1] , len(entry[5]) , entry[2] , entry[3] , entry[6] , entry[4] , overlapGenes))
 
-print('Libraries searched.')
+    print('Libraries searched.')
 
-#Sort entries by their combined score
-sortedEntries = sorted(entries, key=lambda entry: entry.score , reverse=True)
+    #Sort entries by their combined score
+    sortedEntries = sorted(entries, key=lambda entry: entry.score , reverse=True)
 
-#Iterate over entries and print each entry
+    #Iterate over entries and print each entry
 
-for entry in sortedEntries:
-    if int(entry.overlapGenes) >= int(minOverlap):
-        ofile.write(entry.toString())
+    row = 1
+    for entry in sortedEntries:
+        if int(entry.overlapGenes) >= int(minOverlap):
+            worksheet.write(row , 0 , entry.geneSet)
+            worksheet.write(row , 1 , entry.term)
+            worksheet.write(row , 2 , str(entry.overlapGenes)+'_'+str(module.numGenes))
+            worksheet.write(row , 3 , entry.pval)
+            worksheet.write(row , 4 , entry.zscore)
+            worksheet.write(row , 5 , entry.adjPval)
+            worksheet.write(row , 6 , entry.score)
+            worksheet.write(row , 7 , entry.genes)
+            row += 1
 
-print('%s written.' % oFilePath)
+    print('%s written.' % module.name)
 
 ifile.close()
 ofile.close()
+
+print('\n\n%s written. All done!' % oFilePath)
